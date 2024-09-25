1316072682baa1925c4faf197bb957dde9af9e46
Vinh La <kietla@live.co.uk>
2024-09-25 12:24:06 +0100

    saving legend position to backend

---
+++ travel_app_api back-end/app/controllers/legend_positions_controller.rb
@@ -0,0 +1,23 @@
+class LegendPositionsController < ApplicationController
+    def show
+      # Assuming you only have one position saved for the legend
+      legend_position = LegendPosition.first_or_create(x: 0, y: 0)
+      render json: { x: legend_position.x, y: legend_position.y }
+    end
+  
+    def update
+      legend_position = LegendPosition.first_or_create
+      if legend_position.update(legend_position_params)
+        render json: { x: legend_position.x, y: legend_position.y }
+      else
+        render json: { error: 'Unable to update position' }, status: :unprocessable_entity
+      end
+    end
+  
+    private
+  
+    def legend_position_params
+      params.require(:legend_position).permit(:x, :y)
+    end
+  end
+  
---
+++ travel_app_api back-end/test/models/legend_position_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class LegendPositionTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
