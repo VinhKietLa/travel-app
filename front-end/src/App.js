3634991a665683686fe58ffd87002b2c75fa1f89
Vinh La <kietla@live.co.uk>
2024-10-01 15:10:02 +0100

    working on mobile styles

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,23 +3,22 @@ import Globe from "./components/Globe";
 import Modal from "react-modal";
 import { LoginComponent } from "./components/LoginComponent";
 import { useState, useEffect } from "react";
+
 Modal.setAppElement("#root");
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // New state for toggling the login menu
 
   useEffect(() => {
     const token = localStorage.getItem("token");
-    if (token) {
-      setIsAuthenticated(true);
-    } else {
-      setIsAuthenticated(false);
-    }
+    setIsAuthenticated(!!token);
   }, []);
 
   const handleLogin = (token) => {
     localStorage.setItem("token", token);
     setIsAuthenticated(true);
+    setIsMenuOpen(false); // Close menu on login
   };
 
   const handleLogout = () => {
@@ -27,14 +26,25 @@ function App() {
     setIsAuthenticated(false);
   };
 
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev); // Toggle menu state
+  };
+
   return (
     <div className="App">
       <div className="title">
         <h1>Vinh's Travel Map</h1>
-        {!isAuthenticated && (
-          <div className="login-container">
-            <LoginComponent setIsAuthenticated={handleLogin} />
-          </div>
+        {/* Burger Icon */}
+        <div className="burger-icon" onClick={toggleMenu}>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
+      </div>
+      <div className="login-container">
+        {/* Show LoginComponent if menu is open and user is not authenticated */}
+        {!isAuthenticated && isMenuOpen && (
+          <LoginComponent setIsAuthenticated={handleLogin} />
         )}
         {isAuthenticated && (
           <button onClick={handleLogout} className="logout-button">
@@ -42,7 +52,6 @@ function App() {
           </button>
         )}
       </div>
-
       <Globe isAuthenticated={isAuthenticated} />
     </div>
   );
