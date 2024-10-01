bac8691e9d36af9dcc35567501d9de3e03309b0c
Vinh La <kietla@live.co.uk>
2024-10-01 01:01:52 +0100

    updating CORS URL

--- travel_app_api back-end/config/initializers/cors.rb
+++ travel_app_api back-end/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'  # Your React app's origin
+    origins 'https://travel-app-1-m9f2.onrender.com'  # Your React app's origin
     resource '*',
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options, :head],
